4060bf9e86de7dc8efa8825b0bb1be62ffadf880
maximmonin <maximmonin@gmail.com>
2020-08-19 07:38:40 +0300

    Added warden hooks on login/logout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,8 +4,6 @@ class WelcomeController < ApplicationController
 # Init Vue.js Vuex.Stote
   def initstore
     user = current_user
-    cookies.encrypted[:user_id] = (user.present? && user.id) || nil
-
     respond_to do |format|
       msg = { :lang => I18n.locale, :user => user, :baseurl => request.base_url, :servertime => DateTime.now.strftime("%Q") }
       format.json  { render :json => msg } 
