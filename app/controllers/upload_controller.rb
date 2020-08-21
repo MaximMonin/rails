156752a68cec491339449fe87586c7bf8e73df9b
maximmonin <maximmonin@gmail.com>
2020-08-21 17:14:05 +0300

    Added dropzone mass file uploader to userfolder

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,11 +1,27 @@
 class UploadController < ApplicationController
-  def new
-    FileUtils.mkdir_p('/app/storage/cache')
-    FileUtils.mkdir_p('/app/storage/myfiles')
+  def new  
+    user = current_user.id
+    FileUtils.mkdir_p( Rails.root.to_s + '/storage/cache')
+    FileUtils.mkdir_p( Rails.root.to_s + '/storage/cdn/user' + user.to_s)
+
     uploader = MyFilesUploader.new
     uploader.store!(params[:file])
-    render json: {path: '/', name: uploader.url}
+    mime_type = uploader.content_type
+    filesize = uploader.size
+
+    # Moving files to users dir, CarrierWave cant use current_user
+    filenew = URI.decode_www_form_component(uploader.url)
+    filepreview = URI.decode_www_form_component(uploader.preview.url) if mime_type.start_with? 'image'
+    filenew = filenew[5, filenew.length]
+    filepreview = filepreview[5, filepreview.length] if filepreview.present?
+    FileUtils.mv(Rails.root.to_s + '/storage/cdn/' + filenew, Rails.root.to_s + '/storage/cdn/user' + user.to_s + '/')
+    FileUtils.mv(Rails.root.to_s + '/storage/cdn/' + filepreview, Rails.root.to_s + '/storage/cdn/user' + user.to_s + '/') if filepreview.present?
+    filepreview = filepreview || ''
+
+    render json: {path: '/cdn/user' + user.to_s, name: filenew, mime_type: mime_type, filesize: filesize, preview: filepreview}
   end
   def delete
+    filedel = Rails.root.to_s + '/storage' + params[:file]
+    File.delete(filedel) if File.exist?(filedel)
   end
 end
