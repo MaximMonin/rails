fcedeadff0f84faa8885faf2a4fa1654c4479655
maximmonin <maximmonin@gmail.com>
2020-08-24 23:38:02 +0300

    Added immediate upload to ActiveStorage with update of avatar without commit

--- app/javascript/add_other.js
+++ app/javascript/add_other.js
@@ -9,3 +9,5 @@ window.axios = require('axios');
 const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.headers.common['X-CSRF-Token'] = csrf;
+
+window.ImmediateUploader = require('./upload');
