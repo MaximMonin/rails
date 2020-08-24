db4b2a85bed742e42744bc6a7ae2d5887a70226f
maximmonin <maximmonin@gmail.com>
2020-08-25 00:37:57 +0300

    create variant of avatar for fast uploads

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -2,7 +2,7 @@ class RegistrationController < Devise::RegistrationsController
   def update
     super
     @user = current_user
-    @user.avatar = url_for(@user.photo) if @user.photo.attached?
+    @user.avatar = url_for(@user.photo.variant(resize_to_limit: [300, 300]).processed) if @user.photo.attached?
     @user.save
   end
 end 
