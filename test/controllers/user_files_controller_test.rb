1a38a8326f34a4e7f77948a5c5c92852746e9449
maximmonin <maximmonin@gmail.com>
2020-09-01 18:58:28 +0300

    added some integration tests for controllers

--- test/controllers/user_files_controller_test.rb
+++ test/controllers/user_files_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserFilesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "visit files without login" do
+    get user_file_url ('photos')
+    assert_equal 401, status
+  end
+  test "visit user photo with login" do
+    sign_in_as users(:one)
+    get user_file_url ('photos')
+    assert_equal 200, status
+  end
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "visit home" do
+    get "/"
+    assert_equal 200, status
+    assert_select 'h1', "Welcome to Ruby on rails + Vue.js app"
+  end
+  test "init vue store" do
+    get initstore_url, xhr: true
+
+    r = JSON.parse(@response.body, symbolize_names: true)
+    assert_equal 'en', r [:lang]
+    assert_equal 'http://' + ENV['VIRTUAL_HOST'], r[:baseurl]
+    assert_equal 'http://' + ENV['VIRTUAL_HOST'], r[:baseurl]
+  end
 end
