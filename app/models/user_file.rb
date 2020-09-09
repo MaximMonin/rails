dcbc1bdb936a5a25e4b0f7a41fcbb6788981be5c
maximmonin <maximmonin@gmail.com>
2020-09-09 14:52:26 +0300

    added field files_updated_at and models triggers on user_files changes

--- app/models/user_file.rb
+++ app/models/user_file.rb
@@ -5,4 +5,15 @@ class UserFile < ApplicationRecord
   validates :filesize, numericality: { greater_than: 0 }
 
   belongs_to :user, inverse_of: :user_files
+
+  after_save do |user_files|
+    user = User.find(user_files.user_id)
+    user.files_updated_at = DateTime.now
+    user.save
+  end 
+  after_destroy do |user_files|
+    user = User.find(user_files.user_id)
+    user.files_updated_at = DateTime.now
+    user.save
+  end 
 end
